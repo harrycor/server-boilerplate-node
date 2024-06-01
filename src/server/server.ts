2a7bd28f4f3cc09d671dc8e3231878c95d1b25c9
Harry Corradi <purestaticproductions@gmail.com>
2024-06-01 17:41:31 -0400

    initial commit

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,9 +2,31 @@
 
 import express, { Request, Response } from "express";
 import config from "../config";
+import morgan from "morgan";
+// import apiRouter from "./routes";
+// import authRouter from "./routes/auth";
+// import cors from "cors";
+// import { Server as HTTPServer } from "http";
 
 const app = express();
 const port = config.app.port;
+
+// const allowlist = [``];
+
+// const corsOptionsDelegate = function (req: any, callback: any) {
+//   let corsOptions;
+
+//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true, optionsSuccessStatus: 200 }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
+
+// app.use(cors(corsOptionsDelegate));
+app.use(morgan("dev"));
+app.use(express.json({ limit: "6mb" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
